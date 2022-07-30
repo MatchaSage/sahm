ea26022ab4d4e746d12b45132bbaf84d9d375a98
Treyce Wheeler <t.zeryon@gmail.com>
2022-07-29 21:25:45 -0400

    Hide card text content on hover and replace on leave

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let card = document.querySelectorAll('.card.link');
 
 card.forEach(element =>{
     element.addEventListener('mouseenter', function(){
-        element.style.backgroundImage = "url(./images/wallpapersden.com_pixel-4-stock_320x320.jpg)"
+        element.style.backgroundImage = "url(./images/wallpapersden.com_pixel-4-stock_320x320.jpg)";
+        element.style.fontSize = "0";
     })
 
     element.addEventListener('mouseleave', function(){
-        element.style.backgroundImage = "none"
+        element.style.backgroundImage = "none";
+        element.style.fontSize = "3rem";
     })
 })
